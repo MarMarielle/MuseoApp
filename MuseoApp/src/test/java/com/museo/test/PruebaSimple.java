2fc5f949ac149ab320ca2f181152a474d53a3d5d
mariellemejia <mariellemejia@192.168.100.13>
2026-04-06 22:45:43 -0500

    Se agrega configuración Maven (pom.xml) y pruebas JUnit

---
+++ MuseoApp/src/test/java/com/museo/test/PruebaSimple.java
@@ -0,0 +1,12 @@
+package com.museo.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PruebaSimple {
+
+    @Test
+    void pruebaBasica() {
+        assertEquals(2, 1 + 1);
+    }
+}
